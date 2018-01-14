23d6fd6caab9518824b93e287f9291cce3066b83
Tom Plumpton <Thomas.Plumpton@hotmail.co.uk>
2018-01-14 20:13:17 +0000

    init commit

---
+++ js/javascript.js
@@ -0,0 +1,64 @@
+// Image Rollovers
+function imageRollover1() {
+	document.getElementById("Image1").src="images/ImageRollover1.jpg";
+}
+
+function imageRolloff1(){
+	document.getElementById("Image1").src="images/gallery_1.jpg";
+}
+
+<!-----------------------------Gallery Image 2------------------------------->
+
+function imageRollover2() {
+	document.getElementById("Image2").src="images/ImageRollover2.jpg";
+}
+
+function imageRolloff2(){
+	document.getElementById("Image2").src="images/gallery_2.jpg";
+}
+
+<!-----------------------------Gallery Image 3------------------------------->
+
+function imageRollover3() {
+	document.getElementById("Image3").src="images/ImageRollover3.jpg";
+}
+
+function imageRolloff3(){
+	document.getElementById("Image3").src="images/gallery_3.jpg";
+}
+
+<!-----------------------------Gallery Image 4------------------------------->
+
+function imageRollover4() {
+	document.getElementById("Image4").src="images/ImageRollover4.jpg";
+}
+
+function imageRolloff4(){
+	document.getElementById("Image4").src="images/gallery_4.jpg";
+}
+
+<!-----------------------------Gallery Image 5------------------------------->
+
+function imageRollover5() {
+	document.getElementById("Image5").src="images/ImageRollover5.jpg";
+}
+
+function imageRolloff5(){
+	document.getElementById("Image5").src="images/gallery_5.jpg";
+}
+
+//Image Zoom
+/*
+<!---------------------------------Image 1----------------------------------->
+function imageZoom1() {
+	document.getElementById("Zoom1").style.width="120%";
+	document.getElementById("Zoom1").style.height="120%";
+	document.getElementById("Zoom1").position='fixed';
+}
+
+function imageRestore1() {
+	document.getElementById("Zoom1").style.width="100%";
+	document.getElementById("Zoom1").style.height="100%";
+	document.getElementById("Zoom1").position='fixed';
+}
+*/
